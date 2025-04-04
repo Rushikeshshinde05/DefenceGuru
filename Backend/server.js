b74c448026cbcec218ae54198bfd72ed79b6e5c3
Rushikeshshinde05 <101400130+Rushikeshshinde05@users.noreply.github.com>
2025-04-04 15:39:43 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -117,6 +117,6 @@ app.get('/api/testimonials', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
